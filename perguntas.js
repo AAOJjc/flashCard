2d6bfed36f22f432f8f8c6394c6ca4c92ad2a0a0
AAOJjc <00001108738497sp@al.educacao.sp.gov.br>
2024-11-29 00:04:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Proramação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    'Proramação',
+    'O que são operadores de comparação?',
+    'é utilizado quando é preciso comparar valores ao escrver declarações condicionais.'
+)
+
+criaCartao(
+    'Proramação',
+    'Quais são as variaveis do JavaScript',
+    'elas são: var, const, let.'
+)
